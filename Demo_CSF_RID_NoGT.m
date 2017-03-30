bbd090e5fc49fddf06aba8b0fb6a1edc06c66338
XuJun <nankaimathxujun@gmail.com>
2017-03-30 17:14:03 +0800

    ad

--- Demo_CSF_RID_NoGT.m
+++ Demo_CSF_RID_NoGT.m
@@ -1,63 +1,80 @@
-
 %--------------------------------------------------------------------------
-clc;
 clear;
+%% read  image directory
+% GT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\DJI_Results\Real_MeanImage\';
+% GT_fpath = fullfile(GT_Original_image_dir, '*.JPG');
+% TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\DJI_Results\Real_NoisyImage\';
+% TT_fpath = fullfile(TT_Original_image_dir, '*.JPG');
+% GT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_MeanImage\';
+% GT_fpath = fullfile(GT_Original_image_dir, '*.png');
+% TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_NoisyImage\';
+% TT_fpath = fullfile(TT_Original_image_dir, '*.png');
+% GT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_ccnoise_denoised_part\';
+% GT_fpath = fullfile(GT_Original_image_dir, '*mean.png');
+% TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\cc_Results\Real_ccnoise_denoised_part\';
+% TT_fpath = fullfile(TT_Original_image_dir, '*real.png');
 
-setname          = 'real_image_noise_dataset';
+% 
+% GT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\our_Results\Real_MeanImage\';
+% GT_fpath = fullfile(GT_Original_image_dir, '*.JPG');
+% TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\our_Results\Real_NoisyImage\';
+% TT_fpath = fullfile(TT_Original_image_dir, '*.JPG');
+
+
+GT_im_dir  = dir(GT_fpath);
+TT_im_dir  = dir(TT_fpath);
+im_num = length(TT_im_dir);
+
 method           =  'CSF';
-ref_folder       =  fullfile('C:\Users\csjunxu\Desktop\CVPR2017\crosschannel_CVPR2016\',setname);
+%% write image directory
 
-den_folder       =  ['Results_',setname,'_',method];
-if ~isdir(den_folder)
-    mkdir(den_folder)
+write_sRGB_dir = ['C:/Users/csjunxu/Desktop/CVPR2017/our_Results/'];
+if ~isdir(write_sRGB_dir)
+    mkdir(write_sRGB_dir)
 end
 
-noise_levels     =  [15];
-images           =  dir(fullfile(ref_folder,'*.png'));
 format compact;
-
 modelname = 'csf_7x7';
-
-for i = 1 : numel(images)
-    [~, name, exte]  =  fileparts(images(i).name);
-    I =   im2double(imread( fullfile(ref_folder,images(i).name) ));
-    [h,w,ch] = size(I);
-    % color or gray image
-    if ch==1
-        IMin_y = I;
-    else
-        % change color space, work on illuminance only
-        IMin_ycbcr = rgb2ycbcr(I);
-        IMin_y = IMin_ycbcr(:, :, 1);
-        IMin_cb = IMin_ycbcr(:, :, 2);
-        IMin_cr = IMin_ycbcr(:, :, 3);
-    end
-    for j = 1 : numel(noise_levels)
-        disp([i,j]);
-        nSig               =    noise_levels(j);
-        load(fullfile('models','table1',['sigma',num2str(nSig)],modelname));
+for nSig     =  [15]
+    PSNR = [];
+    SSIM = [];
+    CCPSNR = [];
+    CCSSIM = [];
+    RunTime = [];
+    for i = 1 : im_num
+        IM =   im2double(imread( fullfile(TT_Original_image_dir,TT_im_dir(i).name) ));
+        IM_GT = im2double(imread(fullfile(GT_Original_image_dir, GT_im_dir(i).name)));
+        S = regexp(TT_im_dir(i).name, '\.', 'split');
+        IMname = S{1};
+        [h,w,ch] = size(IM);
+        fprintf('%s: \n',TT_im_dir(i).name);
+        CCPSNR = [CCPSNR csnr( IM*255,IM_GT*255, 0, 0 )];
+        CCSSIM = [CCSSIM cal_ssim( IM*255, IM_GT*255, 0, 0 )];
+        fprintf('The initial PSNR = %2.4f, SSIM = %2.4f. \n', CCPSNR(end), CCSSIM(end));
+        % color or gray image
+        IMout = zeros(size(IM));
         %         randn('seed',0);
         %         noise_img          =   I+ nSig*randn(size(I));
-        
-        IMout_y = csf_predict(model,IMin_y*255);
-        %         PSNR_value = csnr(uint8(I),uint8(im{end}),0,0);
-        %         PSNR(i,j) = PSNR_value;
+        time0 = clock;
+        load(fullfile('models','table1',['sigma',num2str(nSig)],modelname));
+        for cc = 1:ch
+            %% denoising
+            IMoutcc = csf_predict(model,IM(:,:,cc)*255);
+            IMout(:,:,cc) = IMoutcc{end};
+        end
+        RunTime = [RunTime etime(clock,time0)];
+        fprintf('Total elapsed time = %f s\n', (etime(clock,time0)) );
+        PSNR = [PSNR csnr( IMout, IM_GT*255, 0, 0 )];
+        SSIM = [SSIM cal_ssim( IMout, IM_GT*255, 0, 0 )];
+        fprintf('The final PSNR = %2.4f, SSIM = %2.4f. \n', PSNR(end), SSIM(end));
+        %% output
+        imwrite(IMout/255, [write_sRGB_dir 'Real_' method '/' method '_our_' IMname '.png']);
     end
-    if ch==1
-        IMout = IMout_y{end}/255;
-    else
-        IMout_ycbcr = zeros(size(I));
-        IMout_ycbcr(:, :, 1) = IMout_y{end}/255;
-        IMout_ycbcr(:, :, 2) = IMin_cb;
-        IMout_ycbcr(:, :, 3) = IMin_cr;
-        IMout = ycbcr2rgb(IMout_ycbcr);
-    end
-        imwrite(IMout, ['C:\Users\csjunxu\Desktop\CVPR2017\1_Results\Real_CSF\CSF_Real_' num2str(noise_levels) '_' name '.png']);
-    
+    mPSNR = mean(PSNR);
+    mSSIM = mean(SSIM);
+    mCCPSNR = mean(CCPSNR);
+    mCCSSIM = mean(CCSSIM);
+    mRunTime = mean(RunTime);
+    matname = sprintf([write_sRGB_dir method '_our' num2str(im_num) '.mat']);
+    save(matname,'PSNR','SSIM','mPSNR','mSSIM','RunTime','mRunTime');
 end
-
-% mean_value = mean(PSNR,1);
-
-% save(['PSNR_',setname,'_',method],'noise_levels','PSNR','mean_value');
-
-
